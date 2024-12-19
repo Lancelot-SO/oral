7a1680dc7d609ba85d36bc33a7ac2c4718b208c9
unknown <fsowah001@gmail.com>
2024-12-19 17:36:03 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import './App.css'
-import banner from "./assets/oralbanner.png"
+/* eslint-disable no-unused-vars */
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import banner from "./assets/oralbanner.png";
+import bannermobile from "./assets/bannermobile.png"
 import { FaPhoneAlt, FaWhatsapp, FaEnvelope } from "react-icons/fa";
-
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function App() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_9qhefcl', 'template_gweq0p9', form.current, {
+        publicKey: 'aV-FvEfOZg7fbxTN2',
+      })
+      .then(
+        () => {
+          toast.success('Message sent successfully!');
+        },
+        (error) => {
+          toast.error('Failed to send message. Please try again.');
+        },
+      );
+    e.target.reset();
+  };
 
   return (
     <div>
+      <ToastContainer />
       <div className='relative'>
+        {/* Desktop Banner */}
         <img src={banner} alt='banner'
           loading='lazy'
-          className='w-full h-[100vh]'
+          className='hidden lg:block w-full h-[100vh]'
+        />
+        {/* Mobile Banner */}
+        <img src={bannermobile} alt='banner'
+          loading='lazy'
+          className='lg:hidden block w-full h-[100vh]'
         />
       </div>
-      <div className='absolute top-[170px] right-[130px]'>
-        <div className="flex  ">
-          <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+
+      <div className='absolute lg:top-[170px] lg:left-[1000px] top-[45%] left-[0%] md:left-[5%] md:top-[45%] right-0 sm:left-[10%] sm:top-[50%]'>
+        <div className="flex justify-center">
+          <div className="bg-white/60 backdrop-blur-md shadow-md rounded-lg p-6 w-full max-w-md">
             <h1 className="text-2xl font-bold text-center mb-6">
               Drop Anonymous Tip
             </h1>
-            <form>
+            <form ref={form} onSubmit={sendEmail} encType="multipart/form-data">
               <div className="mb-4">
                 <label
                   htmlFor="message"
-                  className="block text-lg font-medium text-gray-700"
+                  className="block text-lg font-medium text-black"
                 >
                   Your Anonymous Message <b className='text-red-500'>*</b>
                 </label>
                 <textarea
                   id="message"
+                  name="message"
                   rows="4"
                   placeholder="Type your message here..."
                   required
@@ -38,14 +71,15 @@ function App() {
               <div className="mb-4">
                 <label
                   htmlFor="file"
-                  className="block text-lg font-medium text-gray-700"
+                  className="block text-lg font-medium text-black"
                 >
                   Attach a Document (optional)
                 </label>
                 <input
                   type="file"
                   id="file"
-                  className="mt-2 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-purple-50 file:text-green-700 hover:file:bg-purple-100"
+                  name="file"
+                  className="mt-2 block w-full text-sm text-black file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-purple-50 file:text-green-700 hover:file:bg-purple-100"
                 />
               </div>
               <button
@@ -56,8 +90,7 @@ function App() {
               </button>
 
               <div className="mt-6 border-t pt-4">
-
-                <ul className="text-sm text-gray-600 space-y-2">
+                <ul className="text-sm text-black space-y-2">
                   <li className="flex items-center">
                     <FaPhoneAlt className="text-green-600 mr-2" />
                     <span className="font-bold">Tel:</span> 0800900111
@@ -82,8 +115,9 @@ function App() {
           </div>
         </div>
       </div>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'msm': '360px',
+        'xxsm': '390px',
+        'xsm': '375px',
+        'vsm': '393px',
+        'rsm': '412px',
+        'nsm': '414px',
+        'small': '430px',
+        'tablet': '570px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        'large': '1343px',
+        '2xl': '1536px',
+        'xxl': '1620px',
+        'vl': '1728px',
+        '3xl': '1800px',
+        '4xl': '1920px',
+      },
+    },
   },
   plugins: [],
 }
